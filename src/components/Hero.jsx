3f9c299c2ba7c00a7e93e4780424cc4af577ed18
Nazish1712 <nazish1712@gmail.com>
2026-05-01 19:22:55 +0530

    finished making the landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import {ChevronDown} from "lucide-react"
+import {ChevronDown , Sparkles , ArrowRight, Play} from "lucide-react"
 import {useState} from "react"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { nightOwl } from "react-syntax-highlighter/dist/esm/styles/prism"
@@ -13,13 +13,41 @@ const Hero = () => {
             <div className="absolute bottom-20 right-4 sm:right-10 w-64 sm:w-96 h-64 sm:h-96 bg-cyan-500/10 rounded-full blur-3xl animate-pulse delay-1000"></div>
 
             <div className="max-w-7xl mx-auto text-center relative w-full">
+                <div className="max-w-7xl mx-auto flex flex-col lg:grid lg:grid-cols-2 text-center lg:text-left gap-6 sm:gap-8 lg:gap-12 items-center relative">
                 <div>
-                
+                    <div className="inline-flex items-center space-x-2 px-3 sm:px-4 py-2 bg-blue-500/10 border border-blue-500/20 rounded-full mb-4 sm:mb-6 animate-in slide-in-from-bottom duration-700">
+                        <Sparkles className="w-4 h-4 text-blue-400"/>
+                        <span className="text-xs sm:text-sm text-blue-300">Introducing CodeFlow AI</span>
+                    </div>
+                  <h1 className="text-5xl sm:text-3xl md:text-4xl lg:text-5xl 
+                  xl:text-6xl font-semibold mb-4 sm:mb-6 animate-in slide-in-from-bottom duration-700 delay-100 leading-tight">
+                    <span className="bg-gradient-to-r from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent block mb-1 sm:mb-2 ">Code Faster</span>
+                    <span className="bg-gradient-to-b from-blue-400 via-cyan-400 to-blue-400 bg-clip-text text-transparent block mb-1 sm:mb-2 ">Build Better</span>
+                    <span className="bg-gradient-to-r from-white via-blue-100 to-cyan-100 bg-clip-text text-transparent block mb-1 sm:mb-2 ">With CodeFlow AI</span>
+                  </h1>
+                 <p className="text-md sm:text-base lg:text-lg text-gray-400 max-w-2xl mx-auto lg:mx-0 mb-6 sm:mb-8 animate-in slide-in-from-bottom duration-700 delay-200 leading-relaxed">Accelerate your development workflow with intelligent code completion , automated testing, and smart debugging. Ship production-ready code 10x faster
+                </p>
+
+                <div className="flex flex-col sm:flex-row items-center justify-center lg:justify-start gap-3 sm:gap-4 mb-8 sm:mb-12 animate-in slide-in-from-bottom duration-700 delay-300">
+                    <button className="group w-full sm:w-auto px-6 sm:px-8 py-3 sm:py-4 bg-gradient-to-b from-blue-600 to-blue-400 rounded-lg font-semibold text-sm sm:text-base transition-all duration-300 hover:scale-102 flex items-center justify-center space-x-2">
+                        <span>Start Coding Free</span>
+                        <ArrowRight className="w-4 h-4 sm:h-5 group-hover:translate-x-1 transition-transform duration-300"/>
+                    </button>
+
+            
+                <button className="group w-full sm:w-auto px-6 sm:px-8 py-3 sm:py-4 bg-white/5 backdrop:blur-sm border border-white/10 rounded-lg font-semibold text-sm sm:text-base transition-all duration-300 hover:bg-white/10 flex items-center justify-center space-x-2">
+                        <div className="p-2 bg-white/10 rounded-full group-hover:bg-white/20 duration-300 transition-colors">
+                        <Play className="w-4 h-4 sm:w-5 sm:h-5 fill-white"/>
+                        </div>
+                        <span>Watch Demo</span>
+                    </button>
+                </div>
+                </div>
 
 
             <div className="relative order-2 w-full">
                 <div className="relative bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl p-3 sm:p-4 shadow-2xl border border-white/10">
-                    <div className="bg-gradient-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[280px] sm:w-[350px] lg:h-[450px] border border-white/5">
+                    <div className="bg-gradient-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[280px] sm:h-[350px] lg:h-[450px] border border-white/5">
                         {/**IDE header */}
                         <div className="flex items-center justify-between px-3 sm:px-4 py-2 sm:py-3 bg-white/5 backdrop-blur-sm border-b border-white/10">
                         <div className="flex space-x-2 items-center">
@@ -30,7 +58,7 @@ const Hero = () => {
                            </div>
                            <span className="text-xs sm:text-sm text-gray-300">CodeFlow AI</span>
                         </div>
-                        <ChevronDown className="w-3 h-3 sm:w-4 h-4 text-gray-400"/>
+                        <ChevronDown className="w-3 h-3 sm:w-4 sm:h-4 text-gray-400"/>
                         </div>
 
                         <div className="p-3 sm:p-4 relative h-full">
