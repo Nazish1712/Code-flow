08eb34b76c63f4ebd295b4ae0bc7fe562b3da836
Nazish1712 <nazish1712@gmail.com>
2026-08-12 13:31:37 +0530

    added pb to herso section and arranges width of window div in md

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
     
     const currentFloatingCard = floatingCards[activeTab]
     return (
-        <section className="relative  min-h-screen flex items-center justify-center pt-16 sm:pt-20 px-4 sm:px-6 lg:px-8 overflow-hidden">
+        <section className="relative  min-h-screen flex items-center justify-center pt-16 sm:pt-20 px-4 sm:px-6 lg:px-8 overflow-hidden pb-5">
             <div className="absolute top-20 left-4 sm:left-10 w-48 sm:w-72 h-48 sm:h-72 bg-blue-500/10 rounded-full blur-3xl animate-pulse"></div>
             <div className="absolute bottom-20 right-4 sm:right-10 w-64 sm:w-96 h-64 sm:h-96 bg-cyan-500/10 rounded-full blur-3xl animate-pulse delay-1000"></div>
 
@@ -47,10 +47,10 @@ const Hero = () => {
                 </div>
 
 
-            <div className="relative order-2 w-full ">
+            <div className="relative order-2 w-full md:w-[80%] lg:w-full">
                 <div className="relative bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl p-3 sm:p-4 shadow-2xl border border-white/10">
-                    <div className="bg-gradient-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[290px] sm:h-[450px] md:h[500px] lg:h-[450px] border border-white/5">
-                        {/**IDE header */}
+                    <div className="bg-gradient-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[290px] sm:h-[450px] border border-white/5">
+                     
                         <div className="flex items-center justify-between px-3 sm:px-4 py-2 sm:py-3 bg-white/5 backdrop-blur-sm border-b border-white/10">
                         <div className="flex space-x-2 items-center">
                            <div className="flex space-x-1 sm:space-x-2 items-center">
@@ -68,7 +68,7 @@ const Hero = () => {
                         </div>
 
                         <div className="p-3 sm:p-4 relative h-full">
-                        {/*file tabs*/}
+                        
                         <div className="flex space-x-1 sm:space-x-2 mb-3 sm:mb-4 overflow-x-auto">
                             <button
                             onClick={() => setActiveTab("Find")}
@@ -81,16 +81,12 @@ const Hero = () => {
                             className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${ activeTab === "Book" ? "bg-blue-500/30 text-white border-blue-400/20" : "bg-white/5 text-gray-300 border-white/10 hover:bg-white/10" } transition-all duration-200 whitespace-nowrap`}>Book</button>
                         </div>
 
-                        {/* Code Content*/}
+                      
                         <div className="relative overflow-hidden flex-grow ">
-                            {/* <SyntaxHighlighter language="javascript" style={nightOwl} customStyle={{margin:0, borderRadius:"8px", fontSize:"11px" , lineHeight: "1.4", height:"100%", border: "1px solid #3c3c3c"}}>
-                                {codeExamples[activeTab]}
-                            </SyntaxHighlighter> */}
                             <img src={heroSectionImages[activeTab]} loading="lazy" alt="activeTab" className="m-0 border border-gray-800 rounded-md h-full object-cover"></img>
                         </div>
                     </div>
                     </div>
-                     {/*Floating cards*/}
                     <div className={`hidden lg:block absolute bottom-4 right-4 transform translate-x-8 translate-y-8 w-72 ${currentFloatingCard.bgColor} backdrop-blur-xl rounded-lg p-4 border border-white/20 shadow-2xl`}>
                         <div className="flex items-center space-x-2 mb-2">
                             <div className={`w-6 h-6 ${currentFloatingCard.iconColor} flex items-center justify-center text-sm font-bold`}>{currentFloatingCard.icon}</div>
